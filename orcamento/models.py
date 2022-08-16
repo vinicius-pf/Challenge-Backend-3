c21662b9c7719c146188ad5844990f3c84fb6e8c
Vinicius Fernandes <vinicius-pf@outlook.com>
2022-08-16 09:13:41 -0300

    Iniciando a segunda semana
    Criando as variáveis novas nos bancos de dados para o início da segunda semana.

--- orcamento/models.py
+++ orcamento/models.py
@@ -3,17 +3,29 @@ from django.db import models
 # Create your models here.
 
 class Receita(models.Model):
-    descricao = models.CharField(max_length=200, unique_for_month='data')
-    valor = models.DecimalField(max_digits = 10, decimal_places=2)
-    data = models.DateField()
+    descricao = models.CharField(max_length=200, unique_for_month='data', blank=False)
+    valor = models.DecimalField(max_digits = 10, decimal_places=2, blank=False)
+    data = models.DateField(blank=False)
 
     def __str__(self):
         return self.descricao
 
 class Despesa(models.Model):
-    descricao = models.CharField(max_length=200, unique_for_month='data')
-    valor = models.DecimalField(max_digits = 10, decimal_places=2)
-    data = models.DateField()
+    categorias = (
+        ('A', 'Alimentação'),
+        ('S', 'Saúde'),
+        ('M', 'Moradia'),
+        ('T', 'Transporte'),
+        ('E', 'Educação'),
+        ('L', 'Lazer'),
+        ('I', 'Imprevistos'),
+        ('O', 'Outras')
+    )
+
+    descricao = models.CharField(max_length=200, unique_for_month='data', blank=False)
+    valor = models.DecimalField(max_digits = 10, decimal_places=2, blank=False)
+    data = models.DateField(blank=False)
+    categoria = models.CharField(choices=categorias, default='O')
 
     def __str__(self):
         return self.descricao
